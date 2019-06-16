d233829a91a1737ce4a07e07b2cd6acc3921606a
ci-build <ci-build@bnl.gov>
2019-06-16 23:42:26 +0000

    Updates

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2f064bf',
+    VERSION: '5f77465',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
