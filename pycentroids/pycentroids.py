e59d97e71ab951d0822a06f7c15661039b90f53c
Stuart B. Wilkins <swilkins@bnl.gov>
2019-06-24 12:12:54 -0400

    Added parameter

--- pycentroids/pycentroids.py
+++ pycentroids/pycentroids.py
@@ -2,11 +2,12 @@ import pandas as pd
 from _pycentroids import find_photons as _find_photons
 
 
-def find_photons(images, threshold=200, box=2, pixel_photon=10, overlap_max=0,
-                 sum_min=800, sum_max=1250, return_pixels='none',
-                 return_map=False):
+def find_photons(images, threshold=200, box=2, pixel_photon=10, pixel_bgnd=15,
+                 overlap_max=0, sum_min=800, sum_max=1250,
+                 return_pixels='none', return_map=False):
     _rtn = _find_photons(images=images, threshold=threshold, box=box,
-                         pixel_photon=pixel_photon, overlap_max=overlap_max,
+                         pixel_photon=pixel_photon, pixel_bgnd=pixel_bgnd,
+                         overlap_max=overlap_max,
                          sum_min=sum_min, sum_max=sum_max,
                          return_pixels=return_pixels, return_map=return_map)
 
