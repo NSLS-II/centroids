b6450ee5c32f062fbd1738ae6240666aad421c2a
Stuart B. Wilkins <swilkins@bnl.gov>
2020-05-05 09:17:13 -0400

    Added tag_pixels option

--- pycentroids/pycentroids.py
+++ pycentroids/pycentroids.py
@@ -6,7 +6,7 @@ def find_photons(images, threshold=200, box=2, search_box=1, pixel_photon=9,
                  pixel_bgnd=15, com_photon=9, overlap_max=0, sum_min=800,
                  sum_max=1250, fit_pixels_2d=True, fit_pixels_1d_x=True,
                  fit_pixels_1d_y=True, return_pixels='none',
-                 return_map=False):
+                 return_map=False, tag_pixels=False):
     """Find photons in CCD images and process for sub-pixel center.
 
     Parameters
@@ -55,6 +55,8 @@ def find_photons(images, threshold=200, box=2, search_box=1, pixel_photon=9,
         Option to return array of pixel values
     return_map : bool
         Option to return map of located photons.
+    tag_pixels : bool
+        Option to tag the found photons in the pixel map by setting the MSB
 
     Returns
     -------
@@ -75,7 +77,8 @@ def find_photons(images, threshold=200, box=2, search_box=1, pixel_photon=9,
                          fit_pixels_2d=fit_pixels_2d,
                          fit_pixels_1dx=fit_pixels_1d_x,
                          fit_pixels_1dy=fit_pixels_1d_y,
-                         return_pixels=return_pixels, return_map=return_map)
+                         return_pixels=return_pixels, return_map=return_map,
+                         tag_pixels=tag_pixels)
 
     df = pd.DataFrame(_rtn[0], columns=_rtn[1])
 
