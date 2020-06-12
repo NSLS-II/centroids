42ccd073a72d5d987d32d6476e947929673156b4
Stuart B. Wilkins <swilkins@bnl.gov>
2020-06-12 17:21:14 -0400

    Revert to "-1" to test with Python 3.5

--- pycentroids/tests/test_photons.py
+++ pycentroids/tests/test_photons.py
@@ -8,7 +8,7 @@ import pandas as pd
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
+pd.set_option('display.max_colwidth', -1)
 
 
 @pytest.fixture
